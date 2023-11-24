59de0e3561f0c2745e6c3497a82c1743fdb14559
binhng2803 <binhng2803@gmail.com>
2023-11-25 06:55:25 +0700

    test webcam

--- yolo_model.py
+++ yolo_model.py
@@ -1,6 +1,13 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO('./YOLO-weight/yolov8n.pt')
-result = model('./assets/images/img2.jpg',show=True)
-cv2.waitKey(0)
+# model = YOLO('./YOLO-weight/yolov8n.pt')
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+while True:
+    flag, img = cap.read()
+    cv2.imshow('image', img)
+    cv2.waitKey(1)
+    
