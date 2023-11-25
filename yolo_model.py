ff58cb1b36185b69ddd6bac7bfa9a541f862b2eb
binhng2803 <binhng2803@gmail.com>
2023-11-25 07:00:44 +0700

    test yolov8n with webcam

--- yolo_model.py
+++ yolo_model.py
@@ -1,13 +1,20 @@
 from ultralytics import YOLO
 import cv2
 
-# model = YOLO('./YOLO-weight/yolov8n.pt')
+model = YOLO('./YOLO-weight/yolov8n.pt')
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 while True:
     flag, img = cap.read()
+    results = model(img, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            print(x1, y1, x2, y2)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
     cv2.imshow('image', img)
     cv2.waitKey(1)
     
