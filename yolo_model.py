d9ed13fa19b7c61c1dc0f8b0c26aed690b87617b
binhng2803 <binhng2803@gmail.com>
2023-11-26 17:12:20 +0700

    update gitignore

--- yolo_model.py
+++ yolo_model.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO('./YOLO-weight/yolov8n.pt')
+model = YOLO('yolov8n.pt')
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -14,7 +14,11 @@ while True:
         for box in boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
             print(x1, y1, x2, y2)
+            x, y, w, h = map(int, box.xywh[0])
+            print(x, y, w, h)
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            
+            
     cv2.imshow('image', img)
     cv2.waitKey(1)
     
